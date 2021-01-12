9c24c4d02d624b5f5af9fe6f7f483b97b84755ad
Fawwaz <you@example.com>
2021-01-11 18:24:26 -0700

    Advanced React

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import axios from 'axios';
 import {MovieCard} from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 export class MainView extends React.Component {
-  state = {
-    movies: [],
-    selectedMovie: null
+  constructor() {
+    super();
+// Initial state is set to null
+    this.state = {
+      movies: null,
+      selectedMovie: null,
+      user: null
+    };
   }
     // One of the "hooks" available in a React Component
     componentDidMount() {
@@ -26,21 +35,44 @@ export class MainView extends React.Component {
         selectedMovie: movie
       });
     }
+    /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
     render() {
-      const { movies, selectedMovie } = this.state;
+      const { movies, selectedMovie, user } = this.state;
+      /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
   
       // Before the movies have been loaded
       if (!movies) return <div className="main-view"/>;
   
       return (
-       <div className="main-view">
-        {selectedMovie
-           ? <MovieView movie={selectedMovie}/>
-           : movies.map(movie => (
-             <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-           ))
-        }
-       </div>
+        <div className="main-view">
+          {selectedMovie
+            ? (
+                <Row className="justify-content-md-center">
+                  <Col md={8} style={{border: '1px solid black'}}>
+                    <MovieView movie={selectedMovie} onBackClick={movie => this.onMovieClick(null)} />
+                  </Col>
+                </Row>
+              )
+            : (
+              <Row className="justify-content-md-center">
+                {movies.map(movie => (
+                  <Col md={3}>
+                    <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+                  </Col>
+                ))}
+              </Row>
+            )
+          }
+        </div>
       );
     }
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {MainView} from "./components/main-view/main-view"
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
@@ -8,10 +11,10 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <MainView />
-      </div>
-    );
+        <Container>
+          <MainView />
+        </Container>
+      );
   }
 }
 
